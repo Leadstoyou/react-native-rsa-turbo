4299ae2425449d51ca0ef396a31ff294781e5f01
tuannm <tuannm180220@gmail.com>
2025-09-12 15:41:53 +0700

    feat(ios): add Podfile for RN example

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  dependency: {
-    platforms: {
-      android: {
-        sourceDir: './android',
-      },
-      ios: {
-        podspecPath: './react-native-rsa-turbo.podspec',
+  dependencies: {
+    'react-native-rsa-turbo': {
+      platforms: {
+        android: {
+          sourceDir: './android',
+        },
+        ios: {
+          podspecPath: './react-native-rsa-turbo.podspec',
+        },
       },
     },
   },
